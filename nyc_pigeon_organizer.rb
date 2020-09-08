0621ef57f62914eb9e15ec14d512b413b1c628c4
Katie B <katiebittner1@gmail.com>
2020-09-08 21:16:15 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,13 +1,10 @@
 def nyc_pigeon_organizer(data)
   organized_pigeons = {}
-  
-  #ex color (hash)    #ex. purple (array)
+
   data.each do |attribute_category, attribute_list|
-          
-    #ex. purple - [peter theo lucky]
+
     attribute_list.each do |attribute, pigeons|   
-      
-      #ex. [theo peter lucky]     #ex theo
+
       pigeons.each do |pigeon|
       
         if !organized_pigeons[pigeon]
