6fe8cc2fb91debf1f2c8db5df4745d16441eda8d
Katie B <katiebittner1@gmail.com>
2020-09-08 20:35:32 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -10,15 +10,15 @@ def nyc_pigeon_organizer(data)
       #ex. [theo peter lucky]     #ex theo
       pigeons.each do |pigeon|
       
-        if organized_pigeons[pigeon]
-            if organized_pigeons[pigeon][attribute_category] == nil
+        if organized_pigeons[pigeon] == nil
+          organized_pigeons[pigeon] = {}
+          
+          if organized_pigeons[pigeon][attribute_category] == nil
               puts "successfully found a category to be created"
               organized_pigeons[pigeon][attribute_category] = []
             end
             organized_pigeons[pigeon][attribute_category].push(attribute.to_s)
             puts organized_pigeons[pigeon][attribute_category]
-        else 
-          organized_pigeons[pigeon] = {}
         end
         
       end
