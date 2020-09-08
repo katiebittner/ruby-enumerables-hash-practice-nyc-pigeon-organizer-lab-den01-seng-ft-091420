2d9ad4a3ee2e500485ab8db7256a7995ae9c7b1e
Katie B <katiebittner1@gmail.com>
2020-09-08 21:17:24 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,5 +1,5 @@
 def nyc_pigeon_organizer(data)
-  organized_pigeons = data.each_with_object({}) |attribute_category, attribute_list|
+  organized_pigeons = data.each_with_object({}) do |(attribute_category, attribute_list), organized_pigeons|
 
     attribute_list.each do |attribute, pigeons|   
 
