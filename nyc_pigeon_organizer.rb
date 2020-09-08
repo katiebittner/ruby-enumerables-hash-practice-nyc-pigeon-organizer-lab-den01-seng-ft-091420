cd951f794a3412bfed459a5ca578e61d18b19698
Katie B <katiebittner1@gmail.com>
2020-09-08 19:18:19 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,24 +1,8 @@
 def nyc_pigeon_organizer(data)
   organized_pigeons = {}
-  
   data.each do |key, value|
     value.each do |inner_key, inner_value|
-      if organized_pigeons[inner_value]
-        
-      else
-        organized_pigeons[inner_value] = inner_key
-      end
+      organized_pigeons[inner_value] = inner_key
     end
   end
-  
-  
-  
-end
-
-
-
-if shipping_manifest["top hat"]
-  shipping_manifest["top hat"] += 1
-else
-  shipping_manifest["top hat"] = 1
 end
