a732f2e8b44ac0866ab7c76f42d4780661650e18
Katie B <katiebittner1@gmail.com>
2020-09-08 19:23:47 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,9 +1,19 @@
 def nyc_pigeon_organizer(data)
+  
   organized_pigeons = {}
+  
   data.each do |key, value|
+    
     value.each do |inner_key, inner_value|
-      organized_pigeons[inner_value] = inner_key
+      if organized_pigeons[inner_value]
+      
+      else
+        organized_pigeons[inner_value] = inner_key
+      end
     end
+    
   end
+  
   organized_pigeons
+  
 end
