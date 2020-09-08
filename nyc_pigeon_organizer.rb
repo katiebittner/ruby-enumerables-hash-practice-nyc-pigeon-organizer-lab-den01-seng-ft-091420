66099bd1a67916eae849ce9ee32834ce7d2338a6
Katie B <katiebittner1@gmail.com>
2020-09-08 19:46:08 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,18 +2,19 @@ def nyc_pigeon_organizer(data)
   
   organized_pigeons = {}
   
-  #ex. color  - hash    #ex. list of colors and names - hash
+  #ex color (hash)    #ex. purple (array)
   data.each do |attribute_category, attribute_list|
           
-    #ex. purple - hash  #ex. theo peter jr lucky - array
+    #ex. purple - [peter theo lucky]
     attribute_list.each do |attribute, pigeons|   
       
-      #ex. theo peter lucky     #ex theo
+      #ex. [theo peter lucky]     #ex theo
       pigeons.each do |pigeon|
       
-        if organized_pigeons[pigeon] 
-          organized_pigeons[pigeon][attribute_category] = [attribute]
-        else
+        #if theo is already in the list, adds a category and its value as in color: purple
+        if organized_pigeons[pigeon][attribute_category] 
+          organized_pigeons[pigeon][attribute_category].push(attribute)
+        else if organized_pigeons[pigeon]
           organized_pigeons[pigeon] = {}
           organized_pigeons[pigeon][attribute_category] = [attribute]
         end
