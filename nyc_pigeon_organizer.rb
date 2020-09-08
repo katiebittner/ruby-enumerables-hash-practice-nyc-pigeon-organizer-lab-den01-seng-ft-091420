8d0429308db8e8899d42b853e620f297c4752819
Katie B <katiebittner1@gmail.com>
2020-09-08 21:06:06 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -15,7 +15,7 @@ def nyc_pigeon_organizer(data)
             "#{attribute_category}": [attribute.to_s]
           }
         else
-          organized_pigeons[pigeon]
+          organized_pigeons[pigeon]["#{attribute_category}"] = 
           
       end
     end
