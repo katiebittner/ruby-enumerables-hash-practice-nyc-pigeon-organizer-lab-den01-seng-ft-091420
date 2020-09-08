fa04b8d19089606357f468d91135ec1b3a85cc5d
Katie B <katiebittner1@gmail.com>
2020-09-08 19:52:35 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -13,10 +13,12 @@ def nyc_pigeon_organizer(data)
         #if theo is already in the list and has a color category, adds an additional color
         if organized_pigeons[pigeon][attribute_category] 
           organized_pigeons[pigeon][attribute_category].push(attribute)
+          
         #if theo is in the list but does not have color category, add the key and adds a value
         else if organized_pigeons[pigeon]
           organized_pigeons[pigeon][attribute_category] = [attribute]
         end
+        
       end
     end
   end
